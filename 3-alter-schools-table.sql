44049be159e71f373c5bbb6d0f39dbbd722e64f4
okocsis90 <o.kocsis90@gmail.com>
2017-02-03 14:27:02 +0100

    task 3 finished

--- 3-alter-schools-table.sql
+++ 3-alter-schools-table.sql
@@ -3,3 +3,5 @@ Write an SQL file here, which changes the schools table's schema with the follow
 - adds an address field (varchar), without the NOT NULL constraint
 - modifies the 'name' column's name to 'code'
 */
+ALTER TABLE schools ADD address VARCHAR;
+ALTER TABLE schools RENAME COLUMN name TO code;
