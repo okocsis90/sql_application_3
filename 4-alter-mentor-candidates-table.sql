8426386cbfe44baff41b1468d44123706f45c3f8
okocsis90 <o.kocsis90@gmail.com>
2017-02-03 14:30:16 +0100

    task 4 finished, all tests work as expected

--- 4-alter-mentor-candidates-table.sql
+++ 4-alter-mentor-candidates-table.sql
@@ -2,3 +2,4 @@
 Write an SQL file here, which changes the mentor_candidates table's schema with the following:
 - adds a new autoincremented field with the name 'id'
 */
+ALTER TABLE mentor_candidates ADD id SERIAL;
